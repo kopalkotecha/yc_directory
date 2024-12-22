237ea29a20392f912586269bb3d3d3c4088b3374
portfolio_website <2019kopal.kotecha@ves.ac.in>
2024-12-22 11:59:35 +0530

    initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {auth} from "@/auth";
 
 export default async function Home({searchParams} : {
     searchParams: Promise<{query?: string}>
@@ -12,6 +13,10 @@ export default async function Home({searchParams} : {
 
     // const posts = await client.fetch(STARTUPS_QUERY);
     // console.log(JSON.stringify(posts, null, 2));
+
+
+    const session = await auth();
+    console.log(session?.id);
 
     const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
